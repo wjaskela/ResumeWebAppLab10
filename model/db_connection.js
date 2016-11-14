b74539f8e7a212478427bce42841d0c8d4af175d
wayde <jaskela@sonoma.edu>
2016-11-13 20:05:23 -0800

    added login info for database

--- model/db_connection.js
+++ model/db_connection.js
@@ -1,6 +1,6 @@
 exports.config = {
-    user: '',
-    password: '',
-    host: '',
-    database: ''
+    user: 'jaskela',
+    password: 'Wj003775803',
+    host: 'blue.cs.sonoma.edu',
+    database: 'jaskela'
 };

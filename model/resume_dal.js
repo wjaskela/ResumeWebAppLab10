28db381f5cfab153d5141295af95fd1ef385b126
jaskela <jaskela@sonoma.edu>
2016-11-30 19:59:02 -0800

    updates per Lab 11

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -5,7 +5,7 @@ var db  = require('./db_connection.js');
 var connection = mysql.createConnection(db.config);
 
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM resume;';
+    var query = 'SELECT * FROM resume_view;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
@@ -13,7 +13,7 @@ exports.getAll = function(callback) {
 };
 
 exports.getById = function(resume_id, callback) {
-    var query = 'SELECT * FROM resume WHERE resume_id = ?';
+    var query = 'SELECT * FROM resume_view WHERE resume_id = ?';
     var queryData = [resume_id];
 
     connection.query(query, queryData, function(err, result) {
--- routes/school_routes.js
+++ routes/school_routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var school_dal = require('../model/school_dal');
+var address_dal = require('../model/address_dal');
 
 
 // View All schools

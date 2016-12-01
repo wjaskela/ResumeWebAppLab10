b4919649c8c4c9a94e02c9807ed566e9098980b0
jaskela <jaskela@sonoma.edu>
2016-11-30 19:08:40 -0800

    Updates for Lab11

--- routes/school_routes.js
+++ routes/school_routes.js
@@ -33,4 +33,58 @@ router.get('/', function(req, res){
     }
 });
 
+// Return the add a new school form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    address_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('school/schoolAdd', {'address': result});
+        }
+    });
+});
+
+// View the school for the given id
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.school_name == null) {
+        res.send('School Name must be provided.');
+    }
+    else if(req.query.address_id == null) {
+        res.send('An Address must be selected');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        school_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/school/all');
+            }
+        });
+    }
+});
+
+// Delete a school for the given school_id
+router.get('/delete', function(req, res){
+    if(req.query.school_id == null) {
+        res.send('school_id is null');
+    }
+    else {
+        school_dal.delete(req.query.school_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/school/all');
+            }
+        });
+    }
+});
+
 module.exports = router;

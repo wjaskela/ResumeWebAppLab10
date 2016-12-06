cc2a635d77f3a30fec4d58c171813ece413472b8
wayde <jaskela@sonoma.edu>
2016-12-05 22:37:34 -0800

    Final changes for Lab11

--- model/skill_dal.js
+++ model/skill_dal.js
@@ -20,3 +20,25 @@ exports.getById = function(skill_id, callback) {
         callback(err, result);
     });
 };
+
+exports.insert = function(params, callback) {
+    var query = 'INSERT INTO skill (name, description) VALUES (?, ?)';
+
+    // the question marks in the sql query above will be replaced by the values of the
+    // the data in queryData
+    var queryData = [params.name, params.description];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+};
+
+exports.delete = function(skill_id, callback) {
+    var query = 'DELETE FROM skill WHERE skill_id = ?';
+    var queryData = [skill_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
--- routes/skill_routes.js
+++ routes/skill_routes.js
@@ -33,4 +33,58 @@ router.get('/', function(req, res){
     }
 });
 
+// Return the add a new skill form
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    skill_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('skill/skillAdd', {'skill': result});
+        }
+    });
+});
+
+// View the skill for the given id
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.name == "") {
+        res.send('Skill name must be provided.');
+    }
+    else if(req.query.description == "") {
+        res.send('Description must be provided');
+    }
+    else {
+        // passing all the query parameters (req.query) to the insert function instead of each individually
+        skill_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/skill/all');
+            }
+        });
+    }
+});
+
+// Delete a skill for the given skill_id
+router.get('/delete', function(req, res){
+    if(req.query.skill_id == null) {
+        res.send('skill_id is null');
+    }
+    else {
+        skill_dal.delete(req.query.skill_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/skill/all');
+            }
+        });
+    }
+});
+
 module.exports = router;
